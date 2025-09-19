952a31546dd2c88906fc76ec91f9bedc2267be1d
Stuff1Do <207966845+Stuff1Do@users.noreply.github.com>
2025-09-19 16:57:21 +0800

    organize file structure

---
+++ Components/MainMenuButton.java
@@ -0,0 +1,9 @@
+package Components;
+
+import javax.swing.JButton;
+
+public class MainMenuButton extends JButton {
+    public MainMenuButton() {
+
+    }
+}
--- Windows/MainMenu.java
+++ Windows/MainMenu.java
@@ -1,3 +1,5 @@
+package Windows;
+
 import javax.swing.*;
 import java.awt.*;
 
